77f780aff92f766690e26dbe33db6d4bab1a7381
Tim Summerton-Brier <t.brier93@gmail.com>
2023-12-10 18:48:25 +0000

    Style task-details page

--- src/main/kotlin/com/timsummertonbrier/chores/domain/Trigger.kt
+++ src/main/kotlin/com/timsummertonbrier/chores/domain/Trigger.kt
@@ -3,16 +3,23 @@ package com.timsummertonbrier.chores.domain
 import com.timsummertonbrier.chores.utils.*
 import io.micronaut.serde.annotation.Serdeable
 import kotlinx.datetime.*
+import java.lang.RuntimeException
 import java.time.DateTimeException
 
 interface Trigger {
     val triggerType: TriggerType
+    val friendlyString: String
+    val note: String?
     fun calculateNextDueDate(): LocalDate?
 }
 
 @Serdeable
 data class FixedDelayTrigger(val daysBetween: Int) : Trigger {
     override val triggerType = TriggerType.FIXED_DELAY
+
+    override val friendlyString = "$daysBetween ${if (daysBetween == 1) "day" else "days"} after the last completion"
+
+    override val note = null
 
     override fun calculateNextDueDate(): LocalDate {
         return today().plusDays(daysBetween)
@@ -22,6 +29,10 @@ data class FixedDelayTrigger(val daysBetween: Int) : Trigger {
 @Serdeable
 data class WeeklyTrigger(val dayOfWeek: Int) : Trigger {
     override val triggerType = TriggerType.WEEKLY
+
+    override val friendlyString = "Every week on ${dayOfWeek.dayName()}"
+
+    override val note = null
 
     override fun calculateNextDueDate(): LocalDate {
         val today = today()
@@ -37,6 +48,14 @@ data class WeeklyTrigger(val dayOfWeek: Int) : Trigger {
 @Serdeable
 data class MonthlyTrigger(val dayOfMonth: Int) : Trigger {
     override val triggerType = TriggerType.MONTHLY
+
+    override val friendlyString = "Every month on the ${dayOfMonth.indexName()} day"
+
+    override val note = if (dayOfMonth > 28) {
+        "For months with less than $dayOfMonth days the task will be triggered on the last day of the month"
+    } else {
+        null
+    }
 
     override fun calculateNextDueDate(): LocalDate {
         val today = today()
@@ -71,6 +90,14 @@ data class MonthlyTrigger(val dayOfMonth: Int) : Trigger {
 data class YearlyTrigger(val monthOfYear: Int, val dayOfMonth: Int) : Trigger {
     override val triggerType = TriggerType.YEARLY
 
+    override val friendlyString = "Every year on the ${dayOfMonth.indexName()} of ${monthOfYear.monthName()}"
+
+    override val note = if (monthOfYear == 2 && dayOfMonth == 29) {
+        "On non leap years the task will be triggered on the 28th of February"
+    } else {
+        null
+    }
+
     override fun calculateNextDueDate(): LocalDate {
         val today = today()
         return try {
@@ -104,7 +131,8 @@ data class YearlyTrigger(val monthOfYear: Int, val dayOfMonth: Int) : Trigger {
 @Serdeable
 data class OneOffTrigger(val dummyProperty: String = "data classes must have at least one property") : Trigger {
     override val triggerType = TriggerType.ONE_OFF
-
+    override val friendlyString = "Never"
+    override val note = null
     override fun calculateNextDueDate() = null
 }
 
@@ -114,4 +142,44 @@ enum class TriggerType {
     MONTHLY,
     YEARLY,
     ONE_OFF,
+}
+
+private fun Int.dayName(): String {
+    return when (this) {
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        7 -> "Sunday"
+        else -> throw RuntimeException("Unknown day of week number: $this")
+    }
+}
+
+private fun Int.monthName(): String {
+    return when (this) {
+        1 -> "January"
+        2 -> "February"
+        3 -> "March"
+        4 -> "April"
+        5 -> "May"
+        6 -> "June"
+        7 -> "July"
+        8 -> "August"
+        9 -> "September"
+        10 -> "October"
+        11 -> "December"
+        12 -> "November"
+        else -> throw RuntimeException("Unknown day of week number: $this")
+    }
+}
+
+private fun Int.indexName(): String {
+    return when (this) {
+        1 -> "1st"
+        2 -> "2nd"
+        3 -> "3rd"
+        else -> "${this}th"
+    }
 }
